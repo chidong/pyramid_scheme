1559b5b99760eba1ae109b525dfc22eb3ee691ff
Chi-Dong Ly <lychhh@googlemail.com>
2020-11-23 12:27:34 +0100

    password managenment

--- src/components/PasswordChange/index.tsx
+++ src/components/PasswordChange/index.tsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-const PasswordChange = () => (
-  <div>
-    <h1>PasswordChange</h1>
-  </div>
-);
+import { withFirebase } from "../Firebase";
 
-export default PasswordChange;
+const PasswordChangeForm = (props: any) => {
+  const [passwordOne, setPasswordOne] = useState("");
+  const [passwordTwo, setPasswordTwo] = useState("");
+  const [error, setError] = useState(null);
+
+  const onSubmit = (event: React.FormEvent) => {
+    props.firebase
+      .doPasswordUpdate(passwordOne)
+      .then(() => {
+        setPasswordOne("");
+        setPasswordTwo("");
+        setError(null);
+      })
+      .catch((error: any) => {
+        setError(error);
+      });
+
+    event.preventDefault();
+  };
+
+  const isInvalid = passwordOne !== passwordTwo || passwordOne === "";
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        name="passwordOne"
+        value={passwordOne}
+        onChange={(e) => setPasswordOne(e.currentTarget.value)}
+        type="password"
+        placeholder="New Password"
+      />
+      <input
+        name="passwordTwo"
+        value={passwordTwo}
+        onChange={(e) => setPasswordTwo(e.currentTarget.value)}
+        type="password"
+        placeholder="Confirm New Password"
+      />
+      <button disabled={isInvalid} type="submit">
+        Reset My Password
+      </button>
+
+      {error && <p>{(error as any).message}</p>}
+    </form>
+  );
+};
+
+export default withFirebase(PasswordChangeForm);
--- src/components/PasswordForget/index.tsx
+++ src/components/PasswordForget/index.tsx
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const PasswordForget = () => (
+import { withFirebase } from "../Firebase";
+import * as ROUTES from "../../constants/routes";
+
+const PasswordForgetPage = () => (
   <div>
     <h1>PasswordForget</h1>
+    <PasswordForgetForm />
   </div>
 );
 
-export default PasswordForget;
+const PasswordForgetFormBase = (props: any) => {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(null);
+
+  const onSubmit = (event: React.FormEvent) => {
+    props.firebase
+      .doPasswordReset(email)
+      .then(() => {
+        setEmail("");
+
+        setError(null);
+      })
+      .catch((error: any) => {
+        setError(error);
+      });
+
+    event.preventDefault();
+  };
+
+  const isInvalid = email === "";
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.currentTarget.value)}
+        type="text"
+        placeholder="Email Address"
+      />
+      <button disabled={isInvalid} type="submit">
+        Reset My Password
+      </button>
+
+      {error && <p>{(error as any).message}</p>}
+    </form>
+  );
+};
+
+const PasswordForgetLink = () => (
+  <p>
+    <Link to={ROUTES.PASSWORD_FORGET}>Forgot Password?</Link>
+  </p>
+);
+
+export default PasswordForgetPage;
+
+const PasswordForgetForm = withFirebase(PasswordForgetFormBase);
+
+export { PasswordForgetForm, PasswordForgetLink };
