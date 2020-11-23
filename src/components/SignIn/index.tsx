84a0a391635f8b5d25fa8df6dd5bcc9c97043480
Chi-Dong Ly <lychhh@googlemail.com>
2020-11-23 06:58:15 +0100

    sign in

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -1,9 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { withRouter } from "react-router-dom";
+import { compose } from "recompose";
 
-const SignIn = () => (
+import { SignUpLink } from "../SignUp";
+import { withFirebase } from "../Firebase";
+import * as ROUTES from "../../constants/routes";
+
+const SignInPage = () => (
   <div>
     <h1>SignIn</h1>
+    <SignInForm />
+    <SignUpLink />
   </div>
 );
 
-export default SignIn;
+const SignInFormBase = (props: any) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const onSubmit = (event: React.FormEvent) => {
+    props.firebase
+      .doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        setEmail("");
+        setPassword("");
+        setError(null);
+        props.history.push(ROUTES.HOME);
+      })
+      .catch((error: any) => {
+        setError(error);
+      });
+
+    event.preventDefault();
+  };
+
+  const isInvalid = password === "" || email === "";
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.currentTarget.value)}
+        type="text"
+        placeholder="Email Address"
+      />
+      <input
+        name="password"
+        value={password}
+        onChange={(e) => setPassword(e.currentTarget.value)}
+        type="password"
+        placeholder="Password"
+      />
+      <button disabled={isInvalid} type="submit">
+        Sign In
+      </button>
+
+      {error && <p>{(error as any).message}</p>}
+    </form>
+  );
+};
+
+const SignInForm = compose(withRouter, withFirebase)(SignInFormBase);
+
+export default SignInPage;
+
+export { SignInForm };
