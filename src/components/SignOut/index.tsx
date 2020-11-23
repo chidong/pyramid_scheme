a1a32ad5d84c78b47fefd62c443a5d48eee548aa
Chi-Dong Ly <lychhh@googlemail.com>
2020-11-23 07:05:48 +0100

    sign out

--- src/components/SignOut/index.tsx
+++ src/components/SignOut/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
+import { withFirebase } from "../Firebase";
 
-const SignOut = () => (
-  <div>
-    <h1>SignOut</h1>
-  </div>
+const SignOutButton = (props: any) => (
+  <button type="button" onClick={props.firebase.doSignOut}>
+    Sign Out
+  </button>
 );
-
-export default SignOut;
+export default withFirebase(SignOutButton);
