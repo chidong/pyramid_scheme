740170842aa3446c440c5115a56daa137bfd20bf
Chi-Dong Ly <lychhh@googlemail.com>
2020-11-23 13:31:37 +0100

    authorization

---
+++ src/components/Session/withAuthorization.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import { withRouter } from "react-router-dom";
+import { compose } from "recompose";
+import { withFirebase } from "../Firebase";
+import AuthUserContext from "./context";
+import * as ROUTES from "../../constants/routes";
+
+const withAuthorization = (condition: (authUser: any) => boolean) => (
+  Component: React.FC
+) => {
+  const WithAuthorization = (props: any) => {
+    useEffect(() => {
+      const listener = props.firebase.auth.onAuthStateChanged(
+        (authUser: any) => {
+          if (!condition(authUser)) {
+            props.history.push(ROUTES.SIGN_IN);
+          }
+        }
+      );
+
+      return () => {
+        listener();
+      };
+    });
+
+    return (
+      <AuthUserContext.Consumer>
+        {(authUser) => (condition(authUser) ? <Component {...props} /> : null)}
+      </AuthUserContext.Consumer>
+    );
+  };
+
+  return compose(withRouter, withFirebase)(WithAuthorization);
+};
+
+export default withAuthorization;
