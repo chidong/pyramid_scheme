b460ee975929d5fc9f06567a853fd5d2b9bf75ca
Chi-Dong Ly <lychhh@googlemail.com>
2020-11-24 07:15:35 +0100

    session persistence

--- src/components/Session/withAuthentication.tsx
+++ src/components/Session/withAuthentication.tsx
@@ -5,14 +5,18 @@ import { withFirebase } from "../Firebase";
 
 const withAuthentication = (Component: React.FunctionComponent) => {
   const WithAuthentication = (props: any) => {
-    const [authUser, setAuthUser] = useState(null);
+    const [authUser, setAuthUser] = useState(
+      JSON.parse(localStorage.getItem("authUser") as string)
+    );
 
     useEffect(() => {
       const listener = props.firebase.onAuthUserListener(
         (authUser: any) => {
+          localStorage.setItem("authUser", JSON.stringify(authUser));
           setAuthUser(authUser);
         },
         () => {
+          localStorage.removeItem("authUser");
           setAuthUser(null);
         }
       );
