cc1e4dda594847fc5c44e90062b4e7c5d1ce9472
Chi-Dong Ly <lychhh@googlemail.com>
2020-12-04 17:59:18 +0100

    sign in with react-hook-form

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -5,44 +5,65 @@ import { SignUpLink } from "../SignUp";
 import { PasswordForgetLink } from "../PasswordForget";
 import { FirebaseContext } from "../Firebase";
 import * as ROUTES from "../../constants/routes";
-import { Formik, Form, Field } from "formik";
+import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
-import { TextField } from "formik-material-ui";
-import { Button, LinearProgress } from "@material-ui/core";
+import {
+  Button,
+  Avatar,
+  CssBaseline,
+  TextField,
+  Grid,
+  Typography,
+  Container,
+} from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import { makeStyles } from "@material-ui/core/styles";
+import { useForm } from "react-hook-form";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 interface SignInProps {
   email: string;
   password: string;
 }
 
-const initialValues: SignInProps = {
-  email: "",
-  password: "",
-};
-
 const SignInSchema = Yup.object().shape({
   email: Yup.string().required("required").email(),
   password: Yup.string().required("required"),
 });
 
-const SignInPage = () => (
-  <div>
-    <h1>SignIn</h1>
-    <SignInForm />
-    <br />
-    <SignInGoogle />
-    <PasswordForgetLink />
-    <SignUpLink />
-  </div>
-);
+const SignInPage = () => <SignInForm />;
 
 const SignInFormBase = (props: any) => {
   const [error, setError] = useState(null);
   const firebase = useContext(FirebaseContext);
+  const classes = useStyles();
+  const { register, handleSubmit, errors } = useForm<SignInProps>({
+    resolver: yupResolver(SignInSchema),
+  });
 
-  const handleSubmit = (values: SignInProps, actions: any): void => {
+  const onSubmit = handleSubmit((data) => {
     firebase
-      ?.doSignInWithEmailAndPassword(values.email, values.password)
+      ?.doSignInWithEmailAndPassword(data.email, data.password)
       .then(() => {
         setError(null);
         props.history.push(ROUTES.HOME);
@@ -50,56 +71,78 @@ const SignInFormBase = (props: any) => {
       .catch((error: any) => {
         setError(error);
       });
-    actions.setSubmitting(false);
-    actions.resetForm();
-  };
+  });
 
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={handleSubmit}
-      validationSchema={SignInSchema}
-    >
-      {({ dirty, isValid, isSubmitting, submitForm }) => {
-        return (
-          <Form>
-            <Field
-              component={TextField}
-              name="email"
-              type="email"
-              label="Email"
-            />
-            <br />
-            <Field
-              component={TextField}
-              type="password"
-              label="Password"
-              name="password"
-            />
-            <br />
-            {isSubmitting && <LinearProgress />}
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={onSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            inputRef={register}
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            error={errors.email ? true : false}
+            helperText={errors.email?.message}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            inputRef={register}
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            error={errors.password ? true : false}
+            helperText={errors.password?.message}
+          />
 
-            <br />
-            <Button
-              variant="contained"
-              color="primary"
-              disabled={!dirty || !isValid || isSubmitting}
-              onClick={submitForm}
-            >
-              Sign In
-            </Button>
+          {error && <Alert severity="error">{(error as any).message}</Alert>}
 
-            {error && <p>{(error as any).message}</p>}
-          </Form>
-        );
-      }}
-    </Formik>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <PasswordForgetLink />
+            </Grid>
+            <Grid item>
+              <SignUpLink />
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <SignInGoogle />
+    </Container>
   );
 };
 
 const SignInGoogleBase = (props: any) => {
   const [error, setError] = useState(null);
   const firebase = useContext(FirebaseContext);
+  const classes = useStyles();
 
   const onSubmit = (event: React.FormEvent) => {
     firebase
@@ -126,7 +169,15 @@ const SignInGoogleBase = (props: any) => {
 
   return (
     <form onSubmit={onSubmit}>
-      <button type="submit">Sign In with Google</button>
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        className={classes.submit}
+      >
+        Sign In with Google
+      </Button>
       {error && <p>{(error as any).message}</p>}
     </form>
   );
