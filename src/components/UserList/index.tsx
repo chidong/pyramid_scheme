5540d9ea447167c3cf4d38896e0180ea98a62cdf
Chi-Dong Ly <lychhh@googlemail.com>
2020-12-09 01:03:16 +0100

    remove unused imports

--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { useListVals } from "react-firebase-hooks/database";
 import { FirebaseContext } from "../../components/Firebase";
 import MUIDataTable from "mui-datatables";
